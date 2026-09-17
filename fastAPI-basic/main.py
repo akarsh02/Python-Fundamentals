6b27c62f412cd21558a3ec15d7dd1b88b23d7ad0
akarsh <akarsh8605@gmail.com>
2026-09-17 12:43:24 +0530

    Add path params example

--- fastAPI-basic/main.py
+++ fastAPI-basic/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mockData import products
 
 app = FastAPI()
 
@@ -9,3 +10,20 @@ def home():
 @app.get("/contact") 
 def contact():
     return {"You can reach us at..."}
+
+@app.get("/products")
+def get_products():
+    return products
+
+
+
+## Path Params..
+@app.get("/products/{product_id}")
+def get_one_product(product_id: int):
+    for id in products:
+        if id["id"] == product_id:
+            return id
+        else:
+            return {"message": "Product not found!"}
+
+
